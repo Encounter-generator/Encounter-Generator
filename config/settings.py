28a732be487d36e82c40ad472f84cfa33dfa2c08
Alex Bernardi <64012078+alexbernardi@users.noreply.github.com>
2022-12-17 02:11:29 -0600

    Update settings.py
    using environmental variables from docker.yml

--- config/settings.py
+++ config/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-viibj0n1&i4jw!0+$8z!3qss262nph8!y5#ae-6ury3)gji2&&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
